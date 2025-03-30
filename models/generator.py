dbc18ff7b6ebaf24adcd11c56ee9c05602bae815
phanhieeus <phanhieupkkq@gmail.com>
2025-03-30 19:28:33 +0700

    fix input chanels

--- models/generator.py
+++ models/generator.py
@@ -15,7 +15,7 @@ class ResidualBlock(nn.Module):
         return x + self.residual(x)
 
 class Generator(nn.Module):
-    def __init__(self, BLOCK=16, scale=4):
+    def __init__(self, BLOCK=16, scale=2):
         
         super().__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=9, stride=1, padding=4)
@@ -23,9 +23,10 @@ class Generator(nn.Module):
         self.res_blocks = nn.Sequential(*[ResidualBlock() for _ in range(BLOCK)])
         self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.bn = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 256, kernel_size=3, stride=1, padding=4)
+        self.conv3 = nn.Conv2d(64, 256, kernel_size=3, stride=1, padding=1)
         self.pixel_shuffle = nn.Sequential(*[nn.PixelShuffle(scale) for _ in range(2)])
-        self.conv4 = nn.Conv2d(64, 3, kernel_size=9, stride=1, padding=4)
+        self.conv3_ = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=4)
+        self.conv4 = nn.Conv2d(16, 3, kernel_size=9, stride=1, padding=1)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -37,10 +38,9 @@ class Generator(nn.Module):
         x = self.conv3(x)
         x = self.pixel_shuffle(x)
         x = self.prelu(x)
-        x = self.conv3(x)
-        x = self.pixel_shuffle(x)
+        x = self.conv3_(x)
         x = self.prelu(x)
-        x = self.conv4(x)      
+        x = self.conv4(x)       
         return x
 
     
